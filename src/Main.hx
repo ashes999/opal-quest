695676edf0dba82f35bd0bbeb23bbfc65f9257ee
ashes999 <ashes999@yahoo.com>
2016-07-27 23:47:49 -0400

    Ship, moves to arrow keys

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,14 @@
 
+import luxe.Color;
 import luxe.GameConfig;
 import luxe.Input;
+import luxe.Sprite;
+import luxe.Vector;
 
 class Main extends luxe.Game {
+
+    var ship:Sprite;
+    private static inline var SHIP_VELOCITY:Int = 10;
 
     override function config(config:GameConfig) {
 
@@ -17,6 +23,13 @@ class Main extends luxe.Game {
 
     override function ready() {
 
+        ship = new Sprite({ 
+            name: 'ship',
+            pos: Luxe.screen.mid,
+            color: new Color().rgb(0xff0000),
+            size: new Vector(32, 32)
+        });
+
     } //ready
 
     override function onkeyup( e:KeyEvent ) {
@@ -30,5 +43,20 @@ class Main extends luxe.Game {
     override function update(dt:Float) {
 
     } //update
+    
+    override function onkeydown(event:KeyEvent) {
+        if (event.scancode == luxe.Scan.up)
+        {
+            ship.pos.y -= SHIP_VELOCITY;
+        } else if (event.scancode == luxe.Scan.down) {
+            ship.pos.y += SHIP_VELOCITY;
+        }
+        
+        if (event.scancode == luxe.Scan.left) {
+            ship.pos.x -= SHIP_VELOCITY;
+        } else if (event.scancode == luxe.Scan.right) {
+            ship.pos.x += SHIP_VELOCITY;
+        }
+    }
 
 } //Main
